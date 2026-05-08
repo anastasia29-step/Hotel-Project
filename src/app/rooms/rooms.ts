7dfe7705d9670cff1743931bc491adbaf25136e2
anastasia29-step <stephaniataso@gmail.com>
2026-05-08 20:46:21 +0400

    .

--- src/app/rooms/rooms.ts
+++ src/app/rooms/rooms.ts
@@ -3,7 +3,6 @@ import { Apis } from '../apis';
 import { RouterLink, ActivatedRoute, Params } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { subscribeOn } from 'rxjs';
 
 @Component({
   selector: 'app-rooms',
@@ -12,12 +11,20 @@ import { subscribeOn } from 'rxjs';
   styleUrl: './rooms.css',
 })
 export class Rooms {
-  constructor(public actR: ActivatedRoute, public service: Apis) { }
+  constructor(public actR: ActivatedRoute, public service: Apis) {
+    this.showRooms()
+  }
 
   hotelInfo = signal<any>({})
   hotelRooms = signal<any>([])
 
   showRooms() {
-    this.actR.params.subscribe()
-}
+    this.actR.params.subscribe((data: Params) => {
+      this.service.getHotelid(data['id']).subscribe((roomsData: any) => {
+        console.log(roomsData);
+        this.hotelInfo.set(roomsData)
+        this.hotelRooms.set(roomsData.rooms)
+      })
+    })
+  }
 }
