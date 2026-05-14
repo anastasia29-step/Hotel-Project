8d3a3f4cb8681acb6d10a3fcd80fe7f69951f0f7
anastasia29-step <stephaniataso@gmail.com>
2026-05-14 23:21:31 +0400

    Update details.ts

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,6 +1,6 @@
-import { Component, ElementRef, signal, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, signal, ViewChild } from '@angular/core';
 import { Apis } from '../apis';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
@@ -10,9 +10,12 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './details.css',
 })
 export class Details {
-  constructor(public service: Apis, public actR: ActivatedRoute) {
+  constructor() {
     this.showRoomInfo()
   }
+  public service = inject(Apis);
+  public actR = inject(ActivatedRoute);
+  public router = inject(Router)
   @ViewChild('bookSMS') bookSMS!: ElementRef;
   roomInfo = signal<any>({})
   roomId: any;
@@ -37,32 +40,37 @@ export class Details {
   })
 
   bookRoom() {
-    this.formInfo.value.roomID = this.roomId
-    if (this.formInfo.value.checkInDate < this.formInfo.value.checkOutDate) {
-      this.service.bookRoom(this.formInfo.value).subscribe({
-        next: (data: any) => {
-          console.log(data);
-          this.bookSMS.nativeElement.innerText = data;
-          this.bookSMS.nativeElement.style.backgroundColor = 'darkgreen';
-          this.bookSMS.nativeElement.style.color = 'white'
-          this.bookSMS.nativeElement.classList.add('bookSMSshow');
-          setTimeout(() => {
-            this.bookSMS.nativeElement.classList.remove('bookSMSshow')
-          }, 4000)
-        },
-        error: (error: any) => {
-          console.log(error);
-          this.bookSMS.nativeElement.innerText = error.error;
-          this.bookSMS.nativeElement.style.backgroundColor = 'darkred';
-          this.bookSMS.nativeElement.style.color = 'white';
-          this.bookSMS.nativeElement.classList.add('bookSMSshow');
-          setTimeout(() => {
-            this.bookSMS.nativeElement.classList.remove('bookSMSshow')
-          }, 4000)
-        }
-      })
+    if (this.router) {
+      this.formInfo.value.roomID = this.roomId
+      if (this.formInfo.value.checkInDate < this.formInfo.value.checkOutDate) {
+        this.service.bookRoom(this.formInfo.value).subscribe({
+          next: (data: any) => {
+            console.log(data);
+            this.bookSMS.nativeElement.innerText = data;
+            this.bookSMS.nativeElement.style.backgroundColor = 'darkgreen';
+            this.bookSMS.nativeElement.style.color = 'white'
+            this.bookSMS.nativeElement.classList.add('bookSMSshow');
+            setTimeout(() => {
+              this.bookSMS.nativeElement.classList.remove('bookSMSshow')
+            }, 4000)
+          },
+          error: (error: any) => {
+            console.log(error);
+            this.bookSMS.nativeElement.innerText = error.error;
+            this.bookSMS.nativeElement.style.backgroundColor = 'darkred';
+            this.bookSMS.nativeElement.style.color = 'white';
+            this.bookSMS.nativeElement.classList.add('bookSMSshow');
+            setTimeout(() => {
+              this.bookSMS.nativeElement.classList.remove('bookSMSshow')
+            }, 4000)
+          }
+        })
+      } else {
+        alert("Please, choose available dates")
+      }
     } else {
-      alert("Please, choose available dates")
+      alert ('Please log in to your account first')
     }
+
   }
 }
